4b6b962560dd3814f7dbee065a8ac234e9a75e39
NAGUNWOONG <gallawer@naver.com>
2024-07-22 00:11:18 +0900

    0722

---
+++ java-basic/src/class1/ClasStart1.java
@@ -0,0 +1,22 @@
+package class1;
+
+public class ClasStart1 {
+
+    public static void main(String[] args) {
+
+        String studentName = "학생1";
+        int student1Age = 15;
+        int student1Group = 90;
+
+
+        String studentName2 = "학생2";
+        int student1Age2 = 16;
+        int student1Group2 = 80;
+
+        System.out.println("이름 : " + studentName + " 나이: " + student1Age +
+                            " 성적 : " + student1Group);
+
+        System.out.println("이름 : " + studentName2 + " 나이: " + student1Age2 +
+                " 성적 : " + student1Group2);
+    }
+}
---
+++ java-basic/src/class1/ClassStart2.java
@@ -0,0 +1,19 @@
+package class1;
+
+public class ClassStart2 {
+
+    public static void main(String[] args) {
+
+        String[] studentNames = {"학생1", "학생2","학생3"};
+        int[] studentAge = {15, 16, 20};
+        int[] studentGrade = {90, 80, 100};
+
+        for (int i = 0; i < studentNames.length; i++) {
+            System.out.println("이름 : " + studentNames[i] + " 나이: " + studentAge[i] +
+                    " 성적 : " + studentGrade[i]);
+        }
+
+
+
+    }
+}
